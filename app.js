1cc09eb0dbf39ac447af9b6b6f98e73f95c02720
Eunice Kontor <Eunice.Kontor@ft.com>
2020-12-01 16:46:17 +0000

    posts page now takes message and image

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require("body-parser");
-const user = require("./routes/userRoute");
 
-
-
-// const mongoDbUrl = require("./bin/www");
-// mongoDbUrl();
 
 var homeRouter = require('./routes/home');
 var postsRouter = require('./routes/posts');
@@ -32,7 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 // route setup
 app.use('/', homeRouter);
 app.use('/posts', postsRouter);
-app.use("/user", user);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
