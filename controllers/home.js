22611e519a684e75c112b4a0778ae7e4be6458bc
Christian Naykene <younchriss@live.co.uk>
2020-12-02 12:06:49 +0000

    Login fix

--- controllers/home.js
+++ controllers/home.js
@@ -2,6 +2,19 @@ var HomeController = {
   Index: function(req, res) {
     res.render('home/index', { title: 'Christian' });
   }
+
+//   Login: (function(req, res) {
+//     const {username, password } = req.body;
+//     const user = User.findOne({ username });
+//     const validPassword = bcrypt.compare(password, user.password);
+    
+//     if(validPassword) {
+//         res.send('WELCOME');
+//     } else {
+//         res.send('Try again!');
+//     }
+// })
+
 };
 
 module.exports = HomeController;
