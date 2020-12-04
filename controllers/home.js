9d599dcb6afd023ee2a1a2926e7c88b00fc7e1fa
Hatairy <fanionayo@gmail.com>
2020-12-04 12:05:55 +0000

    css

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,20 @@
+const User = require("../models/user");
+const bcrypt = require('bcrypt');
 var HomeController = {
   Index: function(req, res) {
-    res.render('home/index', { title: 'Acebook' });
-  }
-};
-
-module.exports = HomeController;
+    res.render('home/index');
+  },
+ Login: function(req,res) {
+  const user = User.findOne({ username: req.body.username})
+  user.exec().then(data => {
+      console.log(data);
+      if(!data || !bcrypt.compareSync(req.body.password, data.password)) {
+          res.redirect('/');
+      }
+        req.session.user_id = data._id;
+        // console.log(req.session.user_id)
+        res.redirect('/posts');
+    }
+    )}
+  };
+  module.exports = HomeController;
--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
-
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 var HomeController = require('../controllers/home');
-
 router.get('/', HomeController.Index);
-
+router.post('/', HomeController.Login);
 module.exports = router;
+// router.get('/', HomeController.Login);
+
+// router.post('/', async (req, res) => {
+//     // const {username, password } = req.body;
+//     const username =  req.body.username;
+//     const password = req.body.password;
+//     console.log(password);
+//     const user = await User.findOne({ username }).exec();
+//     console.log(user);
+
+//     let validPassword = bcrypt.compare(password, user.password);
+//     console.log(validPassword);
+
+//     if(validPassword == true) {
+//         res.status(201).redirect('/posts');
+//     } else {
+//         res.status(404);
+//     }
+// })
